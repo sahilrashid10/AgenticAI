e1b56978dcd2b0f13fa6bc6a0ee345a96ce4d4bb
Sahil Rashid <srashid_be22@thapar.edu>
2026-07-28 15:29:10 +0530

    Code ready for multiple pdf's

--- Legal_Document_Review_Agent_HybridRAG/main.py
+++ Legal_Document_Review_Agent_HybridRAG/main.py
@@ -2,44 +2,52 @@ from src.pdf_reader import read_pdf
 from src.chunker import chunk_text
 from src.embeddings import generate_embeddings
 from src.vector_store import store_embeddings
+from src.retriever import hybrid_retrieve
+from src.context_builder import build_context
+from src.chat import ask_llm
 
-document_name, pages = read_pdf(
-    "Legal_Document_Review_Agent_HybridRAG/data/contracts/nda.pdf"
+import os
+import asyncio
+
+pdf_folder = "Legal_Document_Review_Agent_HybridRAG/data/contracts"
+
+all_chunks = []
+
+for filename in os.listdir(pdf_folder):
+
+    if filename.endswith(".pdf"):
+
+        pdf_path = os.path.join(pdf_folder, filename)
+
+        document_name, pages = read_pdf(pdf_path)
+
+        chunks = chunk_text(document_name, pages)
+
+        all_chunks.extend(chunks)
+
+        embeddings = generate_embeddings(chunks)
+
+        store_embeddings(chunks, embeddings)
+
+question = "What is Confidential Information?"
+
+retrieved_chunks = hybrid_retrieve(
+    question,
+    all_chunks,
+    top_k=3
 )
 
-chunks = chunk_text(document_name, pages)
-
-embeddings = generate_embeddings(chunks)
-
-store_embeddings(chunks, embeddings)
-
-from src.retriever import hybrid_retrieve
-
-from src.context_builder import build_context
-
-retrieved_chunks = hybrid_retrieve("What is Confidential Information?", chunks, top_k=3)
-
-print(retrieved_chunks)
-
 context = build_context(retrieved_chunks)
+
 print("=" * 80)
 print(context)
 print("=" * 80)
 
-import asyncio
-
-from src.chat import ask_llm
-
 answer = asyncio.run(
-
     ask_llm(
-
-        "What is Confidential Information?",
-
+        question,
         context
-
     )
-
 )
 
 print(answer)
