0ccbfe00a8b1f1c173ae3b41ec6abcf363a1486c
Sahil Rashid <srashid_be22@thapar.edu>
2026-08-04 10:57:00 +0530

    Implemented event driven programming by simulating an even in main.py.

--- procurement-multi-agent/main.py
+++ procurement-multi-agent/main.py
@@ -3,21 +3,30 @@ import asyncio
 from supervisor import ProcurementSupervisor
 from agents import model_client
 
-purchase_request = """
-We need to purchase 20 Dell laptops for the new engineering team.
-"""
+
+async def on_purchase_request_received(purchase_request):
+
+    supervisor = ProcurementSupervisor()
+
+    result = await supervisor.process_request(
+        purchase_request
+    )
+
+    print("\n========== Final Decision ==========")
+    print(result)
 
 
 async def main():
 
-    supervisor = ProcurementSupervisor()
+    # Simulating an event
+    purchase_request = """
+    We need to purchase 20 Dell laptops
+    for the new engineering team.
+    """
 
-    final_result = await supervisor.process_request(
+    await on_purchase_request_received(
         purchase_request
     )
-
-    print("\n========== Final Result ==========")
-    print(final_result)
 
     await model_client.close()
 
